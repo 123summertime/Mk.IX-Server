1452961046c279653926279f4df3d1148071d332
123summertime <2361011391@qq.com>
2024-06-20 02:10:46 +0800

    增加对audio的volume信息

--- utils/modifier.py
+++ utils/modifier.py
@@ -1,7 +1,7 @@
 import io
 import json
-from math import ceil
 from pydub import AudioSegment
+from pydub.playback import play
 
 from utils.crud import FS
 from schema.message import GetMessageSchema, MessagePayload
@@ -33,7 +33,15 @@ def audioFileMessageModifier(userID: str, groupID: str, message: GetMessageSchem
 
     try:
         audio = AudioSegment.from_file(io.BytesIO(file.file))
-        message.payload.meta = {"length": ceil(len(audio) / 1000)}
+        chunkCount = min(50, (len(audio) // 1000) + 2)
+        chunkLength = len(audio) // chunkCount
+        audioChunks = [audio[i: i + chunkLength].rms for i in range(0, len(audio), chunkLength)]
+        maxVolume = max(audioChunks)
+        volume = [v / maxVolume * 100 // 1 for v in audioChunks]
+        message.payload.meta = {
+            "length": round(len(audio) / 1000, 2),
+            "volume": volume
+        }
         return CheckerState.OK
     except Exception as e:
         return CheckerState.UNKNOWN
